9e09123ac9c690c3e4847ab0e85dea7a71ca59f3
Z User <z@container>
2026-06-11 23:02:13 +0000

    c3e5f261-4eba-45d4-b2e5-e9dc7f8a984c

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
-  cd /home/z/my-project/.next/standalone
-  node server.js
-  echo "[Watchdog] Server died, restarting in 3s..."
+  cd /home/z/my-project
+  node .next/standalone/server.js 2>&1
+  echo "Server crashed, restarting in 3 seconds..."
   sleep 3
 done
