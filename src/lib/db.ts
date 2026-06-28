5b07ca81712f912f2cbb4e1a61954016afcf9af3
Z User <z@container>
2026-06-28 20:13:43 +0000

    76c73e75-bef1-448a-9d79-94341bdcc73b

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,30 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSql } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function createPrismaClient() {
-  const databaseUrl = process.env.DATABASE_URL || ''
-
-  // Turso/libSQL bağlantısı (libsql:// formatında)
-  if (databaseUrl.startsWith('libsql://') || databaseUrl.startsWith('https://')) {
-    const libsql = createClient({
-      url: databaseUrl,
-      authToken: process.env.DATABASE_AUTH_TOKEN || '',
-    })
-    const adapter = new PrismaLibSql(libsql)
-    return new PrismaClient({ adapter })
-  }
-
-  // Yerel SQLite (geliştirme ortamı)
-  return new PrismaClient({
-    log: ['query'],
-  })
-}
-
-export const db = globalForPrisma.prisma ?? createPrismaClient()
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
